a1fc733921c4dfec79b159619bd428fff5840e55
Mrrrp Mreow :3 <mrrrpmreow@gmail.com>
2025-08-23 20:38:08 -0230

    Update TrackBlockMixin.java

--- src/main/java/reign/createaddons/customportalapiintegrations/mixin/TrackBlockMixin.java
+++ src/main/java/reign/createaddons/customportalapiintegrations/mixin/TrackBlockMixin.java
@@ -49,7 +49,7 @@ import com.simibubi.create.content.trains.track.TrackShape;
 import net.minecraft.core.BlockPos;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.level.block.state.BlockState;
-import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraft.world.level.block.state.BlockBehaviour.BlockStateBase;
 import net.minecraft.world.level.block.state.properties.EnumProperty;
 
 @Mixin(TrackBlock.class)
