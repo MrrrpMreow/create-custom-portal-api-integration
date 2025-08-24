2a13454b3a463e6f1a3b4cddcc42d61deabcfa70
Mrrrp Mreow :3 <mrrrpmreow@gmail.com>
2025-08-24 01:31:59 -0230

    wrong mappings :P

--- src/main/java/reign/createaddons/customportalapiintegrations/mixin/TrackBlockMixin.java
+++ src/main/java/reign/createaddons/customportalapiintegrations/mixin/TrackBlockMixin.java
@@ -1,6 +1,6 @@
 package reign.createaddons.customportalapiintegrations.mixin;
 
-import net.minecraft.util.math.random.Random;
+import net.minecraft.util.RandomSource;
 
 import com.google.common.base.Predicates;
 import com.simibubi.create.content.contraptions.glue.SuperGlueEntity;
