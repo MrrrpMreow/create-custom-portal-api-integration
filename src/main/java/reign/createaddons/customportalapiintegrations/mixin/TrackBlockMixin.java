fc796c1d3afc18830bc8bde088f9e6f927582d23
Mrrrp Mreow :3 <mrrrpmreow@gmail.com>
2025-08-24 00:49:17 -0230

    might be an idiot chat 🙏
    remember to refer to the readme :)

--- src/main/java/reign/createaddons/customportalapiintegrations/mixin/TrackBlockMixin.java
+++ src/main/java/reign/createaddons/customportalapiintegrations/mixin/TrackBlockMixin.java
@@ -51,7 +51,6 @@ import net.minecraft.core.BlockPos;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.BlockBehaviour;
-import net.minecraft.world.level.block.state.BlockBehaviour.BlockStateBase;
 import net.minecraft.world.level.block.state.BlockBehaviour.Properties;
 import net.minecraft.world.level.block.state.properties.EnumProperty;
 
@@ -92,8 +91,8 @@ public abstract class TrackBlockMixin {
 			ServerLevel otherLevel = otherSide.getFirst();
 			BlockFace otherTrack = otherSide.getSecond();
 			BlockPos otherTrackPos = otherTrack.getPos();
-			BlockStateBase existing = otherLevel.getBlock(otherTrackPos);
-			if(!existing.isSolid()) {
+			BlockState existing = otherLevel.getBlockState(otherTrackPos);
+			if(!existing.canBeReplaced()) {
 				fail = "blocked";
 				failPos = otherTrackPos;
 				continue;
